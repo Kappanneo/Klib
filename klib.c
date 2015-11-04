447aa77a3f83bd4cc669da45e2c60af80c9c101c
Kappanneo <claudio_pannacci@libero.it>
2015-11-04 19:11:39 +0100

    Copied what I'v done so far.

---
+++ klib.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "klib.h"
+
+int nascii(char n)
+{
+  if(n > 47 && n < 58)
+    return (int) n-48;
+  else
+    return (int) n+48;
+}
+
+int qualcosa(int arg, char* cosa, char* nome)
+{
+  if(arg<=0)
+    {
+      printf("Inserire: %s dopo \"%s \"\n",cosa ,nome);
+      return 1;
+    }
+  else
+    return 0;
+}
+
+long pot(int x, int y)
+{
+  long ret=1;
+  for(int n=0; n < y; n++) 
+    ret*= x;
+  return ret;
+}
+
+long unit(int* num, int cifre)
+{
+  long ret=0;
+  for(int x=0; x < cifre; x++)
+    ret+= pot(10,x)*num[cifre-x-1];
+  return ret;
+}
+
+long deci(char* num, int cifre)
+{
+  long ret=0;
+  for(int x=0; x < cifre; x++)
+    ret+= pot(10,x)*nascii(num[cifre-x-1]);
+  return ret;
+}
---
+++ klib.h
@@ -0,0 +1,5 @@
+int nascii(char); // numeri decimali dai rispettivi caratteri
+int qualcosa(int,char*,char*); // (argc-n (n= quante cose), "quali cose",argv[0])
+long pot(int, int); // pot(x,y)= x^y
+long unit(int*, int); //rende un intero da un array (arg1) di n (arg2) numeri
+long deci(char*, int); // rende un intero da un array (arg1) di n (arg2) caratteri
