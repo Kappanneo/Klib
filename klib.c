454e34223645d8075a4a0ab0a23c1965c0266dac
Kappanneo <claudio_pannacci@libero.it>
2015-11-05 12:49:15 +0100

    Self-casting Pot().

--- klib.c
+++ klib.c
@@ -15,17 +15,17 @@ int qualcosa(int arg, char* cosa, char* nome)
     {
       printf("Inserire: %s dopo \"%s \"\n",cosa ,nome);
       return 1;
-    }
+    } 
   else
     return 0;
 }
 
-long pot(int x, int y)
+long pot(long x, int y)
 {
-  long ret=1;
-  for(int n=0; n < y; n++) 
-    ret*= x;
-  return ret;
+  if(y==0)
+    return 1;
+  else
+    return x*pot(x,y-1);
 }
 
 long unit(int* num, int cifre)
@@ -48,8 +48,6 @@ size_t strl(char* str)
 {
   size_t ret = 0;
   while('\0' != str[ret])
-    {
       ret++;
-    }
   return ret;
 }
--- klib.h
+++ klib.h
@@ -1,6 +1,6 @@
 int nascii(char); // numeri decimali dai rispettivi caratteri
 int qualcosa(int,char*,char*); // (argc-n (n= quante cose), "quali cose",argv[0])
-long pot(int,int); // pot(x,y)= x^y
+long pot(long,int); // pot(x,y)= x^y
 long unit(int*,int); // rende un intero da un array (arg1) di n (arg2) numeri
 long deci(char*,int); // rende un intero da un array (arg1) di n (arg2) caratteri
-size_t strl(char*); // strl( stringa )= lunghazza della stringa
+size_t strl(char*); // strl("stringa")= lunghazza della stringa
